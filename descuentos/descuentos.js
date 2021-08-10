3b31c9e39b0ba2ec1414740f601043322de8d591
Abdias Jimenez <abdiasejr@gmail.com>
2021-08-09 23:53:35 -0300

    cupon feature

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -1,8 +1,26 @@
-const precio = document.getElementById("inputPrecio");
-const descuento = document.getElementById("inputDescuento");
-const resultado = document.getElementById("resultado");
+var precio = document.getElementById("inputPrecio");
+var descuento = document.getElementById("inputDescuento");
+var cupon = document.getElementById("inputCupon");
+var resultado = document.getElementById("resultado");
+
+let cupones = {
+    '': 0,
+    '10OFF': 10,
+    '15OFF': 15,
+    '30OFF': 30,
+    '50OFF': 50,
+    '%FREE%': 100, 
+};
+
 
 function descuentoProducto(){
+
+    Object.values(cupones).forEach( elemento => {
+        if (cupones[cupon.value] == elemento) {
+            descuento.value = Number(descuento.value) + cupones[cupon.value];
+        }
+    });
+
     const compra =  precio.value * (100 - descuento.value) / 100;
     if (compra) {
         return resultado.innerHTML = `Detalle compra: ${compra}$`; 
